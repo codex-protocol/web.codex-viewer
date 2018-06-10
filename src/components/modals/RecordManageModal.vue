e4ad02a9b8de594056e165ee1af487e131d6079d
Shawn Price <shawnp@gmail.com>
2018-06-10 16:21:16 -0700

    WIP. Allow multiple image uploads

--- src/components/modals/RecordManageModal.vue
+++ src/components/modals/RecordManageModal.vue
@@ -5,7 +5,7 @@
     ok-title="Save"
     cancel-variant="outline-primary"
     size="lg"
-    :ok-method="modifyRecord"
+    :ok-method="updateMetadata"
     :on-clear="clearModal"
   >
     <b-form-group
@@ -31,12 +31,24 @@
     <b-form-group
       label="Main Image"
     >
-      <b-img class="main-image" thumbnail fluid :src="mainImage" alt="Main Image" />
+      <b-img
+        class="main-image"
+        thumbnail
+        fluid
+        :src="mainImage"
+        alt="Main Image"
+      />
     </b-form-group>
     <b-form-group
       label="Other Images"
     >
-      <b-img class="main-image" thumbnail fluid :src="mainImage" alt="Main Image" />
+      <b-img
+        class="main-image"
+        thumbnail
+        fluid
+        :src="mainImage"
+        alt="Main Image"
+      />
     </b-form-group>
     <b-form-group
       label="Upload Images"
@@ -45,19 +57,19 @@
         ref="dropzone"
         id="dropzone"
         :options="dropzoneOptions"
-        v-on:vdropzone-file-added="fileAdded"
+        v-on:vdropzone-success="fileAdded"
       />
     </b-form-group>
   </meta-mask-notification-modal>
 </template>
 
 <script>
+import axios from 'axios'
 import vue2Dropzone from 'vue2-dropzone'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
 import { apiUrl } from '../../util/config'
 import callContract from '../../util/web3/callContract'
-import EventBus from '../../util/eventBus'
 import MetaMaskNotificationModal from './MetaMaskNotificationModal'
 
 export default {
@@ -75,16 +87,33 @@ export default {
       name: this.codexRecord.metadata.name,
       description: this.codexRecord.metadata.description,
       mainImage: this.codexRecord.metadata.mainImage.uri,
+      nameHash: null,
+      descriptionHash: null,
+      fileHashes: null,
       dropzoneOptions: {
         url: filesUrl,
+        paramName: 'files',
         thumbnailWidth: 150,
         maxFilesize: 5,
-        uploadMultiple: true,
+        headers: { Authorization: this.$store.state.auth.authToken },
       },
     }
   },
   methods: {
-    fileAdded(file) {
+    addImage(id) {
+      this.images.push({ id })
+    },
+    removeImage(id) {
+      for (let i = 0; i < this.images.length; i++) {
+        if (this.images[i].id === id) {
+          this.images.splice(i, 1)
+        }
+      }
+    },
+    fileAdded(file, response) {
+      const result = response.result[0]
+      const { id } = result
+      this.addImage(id)
     },
     focusModal() {
       this.$refs.defaultModalFocus.focus()
@@ -92,18 +121,39 @@ export default {
     clearModal() {
       Object.assign(this.$data, this.$options.data.apply(this))
     },
-    mounted() {
-      console.log('codexRecord', this.codexRecord)
+    updateMetadata() {
+      const url = `/users/records/${this.tokenId}/metadata`
+      return axios.put(url, {
+        name: this.name,
+        description: this.description,
+        images: this.images,
+      }).then((response) => {
+        const { result, error } = response.data
+        if (error) {
+          throw error
+        } else {
+          const { nameHash, descriptionHash, fileHashes } = result.pendingUpdates[0]
+          this.nameHash = nameHash
+          this.descriptionHash = descriptionHash
+          this.fileHashes = fileHashes
+          return this.modifyRecord()
+        }
+      }).catch((error) => {
+        throw error
+      })
     },
     modifyRecord() {
-      EventBus.$emit('events:record-click-transfer')
-      const input = [this.toEthAddress, this.recordId]
-      return callContract(this.recordContract.approve, input, this.web3)
-        .then(() => {
-          EventBus.$emit('events:record-transfer')
-        })
+      const input = [
+        this.tokenId,
+        this.nameHash,
+        this.descriptionHash,
+        this.fileHashes,
+        1,
+        this.providerMetadataId,
+      ]
+      return callContract(this.recordContract.modifyMetadataHashes, input, this.web3)
         .catch((error) => {
-          console.log('there was an error calling approveTransfer', error)
+          console.error('there was an error calling modifyMetadataHashes', error)
 
           // @NOTE: we must throw the error here so the MetaMaskNotificationModal
           //  can catch() it too
@@ -117,6 +167,19 @@ export default {
     },
     recordContract() {
       return this.web3.recordContractInstance()
+    },
+    tokenId() {
+      return this.codexRecord.tokenId
+    },
+    providerMetadataId() {
+      return this.codexRecord.metadata.id
+    },
+    images() {
+      const images = []
+      for (let i = 0; i < this.codexRecord.metadata.images.length; i++) {
+        images.push({ id: this.codexRecord.metadata.images[i] })
+      }
+      return images
     },
   },
 }
