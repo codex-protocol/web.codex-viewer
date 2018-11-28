066a1a225f628c2b204cef6e171db7164b5faef7
Colin Wood <colinjameswood@gmail.com>
2018-11-28 10:28:49 -0600

    Removes more debug code

--- src/components/CODXBalanceControl.vue
+++ src/components/CODXBalanceControl.vue
@@ -65,7 +65,7 @@ export default {
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 @import "../assets/variables.styl"
 
 .codx-balance-container
@@ -76,10 +76,6 @@ export default {
 
   .balance-wrapper
     flex-grow: 1
-
-  strong
-    font-weight: normal
-    text-decoration: underline
 
   #codx-balance-popover-trigger
     opacity: .8
